f7085ddbc5e5518303a0b8c17a7dbfdecd17792e
Gluuck <ostapenkov74@gmail.com>
2022-03-17 21:17:21 -0700

    fix button github registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,15 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    user = User.where(email: auth['email']).first
-    unless user
-      user = User.create(
-        email: auth['email'],
-        password: Devise.friendly_token[0,20]
-      )
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
       user.create_authorization(auth)
     else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
       user.create_authorization(auth)
     end
     user
--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Authorization, type: :model do
   describe 'associations' do
     it { should belong_to :user }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :provider }
+    it { should validate_presence_of :uid }
+  end
 end
