e87d6b51d28c3111dd28715aba38fdc52a009972
Gluuck <ostapenkov74@gmail.com>
2022-03-22 21:56:53 -0700

    add policy to destroy file

--- app/controllers/resource_files_controller.rb
+++ app/controllers/resource_files_controller.rb
@@ -3,7 +3,8 @@ class ResourceFilesController < ApplicationController
   
   def destroy
     file = ActiveStorage::Attachment.find(params[:id])
-    file.purge if current_user.author?(file.record)
+    authorize file.record
+    file.purge 
     render turbo_stream: turbo_stream.remove(file)
   end
 end
